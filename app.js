814807ad080adbb23ac02b722bf081f28b378d21
YanaUSA <usatiukiana@gmail.com>
2023-04-11 11:25:56 +0300

    fixed mail sending

--- app.js
+++ app.js
@@ -3,29 +3,8 @@ const logger = require("morgan");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const { mongoose } = require("mongoose");
-// const sendEmailFn = require("./services/mailService");
 
 dotenv.config({ path: "./.env" });
-
-const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-const msg = {
-  to: "ciwebip612@duiter.com", // Change to your recipient
-  from: "everesty@meta.ua", // Change to your verified sender
-  subject: "Test email",
-  text: "please verify your email",
-  html: "<strong>Test email from localhost:3000</strong>",
-};
-
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log("Email sent");
-  })
-  .catch((error) => {
-    console.error(error);
-  });
 
 const contactsRouter = require("./routes/api/contactsRoutes");
 const usersRouter = require("./routes/api/usersRoutes");
--- services/mailService.js
+++ services/mailService.js
@@ -10,22 +10,6 @@ const sendEmail = async (data) => {
   } catch (error) {
     console.error(error);
   }
-
-  // const msg = {
-  //   to: "teserat480@djpich.com", // Change to your recipient
-  //   from: "everesty@emeta.ua", // Change to your verified sender
-  //   subject: "Test email",
-  //   text: "please verify your email",
-  //   html: "<strong>Test email from localhost:3000</strong>",
-  // };
-
-  // await sgMail.send(msg)
-  // .then(() => {
-  //   console.log("Email sent");
-  // })
-  // .catch((error) => {
-  //   console.error(error);
-  // });
 };
 
 module.exports = sendEmail;
